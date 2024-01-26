6389a3873b873b06119f439d5032f4106866b6d9
QueenBee <queendolineakpan11@gmail.com>
2024-01-26 17:01:07 +0100

    favicon image added

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body className={`${jakarta.className} dark:bg-black bg-gray-scale`}>
         <Providers>{children}</Providers>
